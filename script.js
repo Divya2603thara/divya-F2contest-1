4109bfa1dbbb4c5059ff45338e2a2e5f5e6b6cc8
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-01-07 17:31:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,31 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  
+  arr.map(printingThroughMap);
+  function printingThroughMap(arrayItem){
+  if(arrayItem.profession === "developer"){
+    console.log(arrayItem);
+  }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(printingThroughForEach);
+function printingThroughForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+        console.log(arrayItem);
+    }
+} 
 }
 
 function addData() {
   //Write your code here
+  let newarr = {id:4,name:"susan",age:"20",profession:"intern"};
+arr.push(newarr);
+console.log(arr);
+  
 }
 
 function removeAdmin() {
@@ -24,4 +40,9 @@ function removeAdmin() {
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [{id:5,name:"mohan",age:"27",profession:"engineer"},
+{id:6,name:"divya",age:"26",profession:"developer"},
+{id:7,name:"sudharsan",age:"22",profession:"programmer"},];
+let concatArray = arr.concat(arr2);
+console.log(concatArray);
 }
